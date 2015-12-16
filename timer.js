a1bd0fd4cfd4c3366c9ce550489633cdd91b43d8
AgnieszkaKurek <agniechakurek@gmail.com>
2015-12-16 17:21:18 +0100

    Adding variables "today", "birthday", "anniversary"

--- timer.js
+++ timer.js
@@ -1,5 +1,21 @@
 ﻿function zeroWiodace(element) {
-    if (element < 10) return element = "0" + element;
-    return = element;
+    if (element < 10)  element = "0" + element;
+   
+}
 
+function pokazOdliczanie(){
+    var today = new Date();
+
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1;
+    var day = today.getDate();
+   
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+
+    var birthday =();
+    
+
+    var anniversary  =  (birthday.getTime() -  today.getTime());
 }
