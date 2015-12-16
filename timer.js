3bae9c6cd5d0892d0ddd29d8d3642e4a206bca52
AgnieszkaKurek <agniechakurek@gmail.com>
2015-12-16 18:41:17 +0100

    Adding new variables

--- timer.js
+++ timer.js
@@ -3,7 +3,7 @@
    
 }
 
-function pokazOdliczanie(){
+function odliczanie(){
     var today = new Date();
 
     var year = today.getFullYear();
@@ -14,8 +14,23 @@ function pokazOdliczanie(){
     var minutes = today.getMinutes();
     var seconds = today.getSeconds();
 
-    var birthday = new Date(2016,0,30,19,0,0,0);
+    var birthday = new Date(2016, 0, 30, 19, 0, 0, 0);
+    var ms_at_day = 24 * 60 * 60 * 1000;
     
 
-    var anniversary  =  (birthday.getTime() -  today.getTime());
+    var anniversary = (birthday.getTime() - today.getTime());
+
+    var eDayTo = anniversary / ms_at_day;
+    var DayTo = Math.floor(eDayTo);
+    var eHoursTo = (eDayTo - DayTo) * 24;
+    var HoursTo = Math.floor(eHoursTo);
+    var eMinutesTo = (eHoursTo - HoursTo) * 60;
+    var MinutesTo = Math.floor(eMinutesTo);
+    var eSecondsTo = (eMinutesTo - MinutesTo) * 60;
+    var SecondsTo = Math.floor(eSecondsTo);
+
+
+    document.getElementById("timer").innerHTML = DayTo + "/" + HoursTo + "/" + MinutesTo + "/" + SecondsTo;
+    setTimeout("odliczanie()", 1000);
+}
 }
